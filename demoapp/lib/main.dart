5eb3cd62a224172665a4bd9815972e71048eeef8
Koishik Ahmed <bitgrap@users.noreply.github.com>
2021-04-18 20:25:04 +0600

    Day 3 completed

--- demoapp/lib/main.dart
+++ demoapp/lib/main.dart
@@ -1,4 +1,6 @@
-import 'package:demoapp/homepage.dart';
+import 'package:demoapp/pages/loginpage.dart';
+
+import 'pages/homepage.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/basic.dart';
 
@@ -11,7 +13,20 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
+      // home: HomePage(),
+      themeMode: ThemeMode.light,
+      theme: ThemeData(
+        primaryColor: Colors.purple,
+      ),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+      ),
+      initialRoute: "/home",
+      routes: {
+        "/home": (context) => HomePage(),
+        "/": (context) => LoginPage(),
+        "/login": (context) => LoginPage(),
+      },
     );
   }
 }
